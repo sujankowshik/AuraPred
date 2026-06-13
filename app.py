37433102f7e1ad41b1f52a4e4191834b493ccad2
Jilla Sujan Kowshik <jillasujankowshik@Jillas-MacBook-Air.local>
2026-06-13 15:00:44 +0530

    fix: swap postgresql driver to pure python pg8000 to prevent pg_config compile errors

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ app = Flask(__name__)
 db_url = os.environ.get('DATABASE_URL')
 if db_url:
     if db_url.startswith("postgres://"):
-        db_url = db_url.replace("postgres://", "postgresql://", 1)
+        db_url = db_url.replace("postgres://", "postgresql+pg8000://", 1)
+    elif db_url.startswith("postgresql://"):
+        db_url = db_url.replace("postgresql://", "postgresql+pg8000://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///housing_app.db'
